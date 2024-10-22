df8cd594660df4f8175cbb026de15023f53b6363
Uriei <uriei@protonmail.com>
2024-10-22 21:38:59 +0200

    Remove heels sync

--- AnimSync/AnimSync.cs
+++ AnimSync/AnimSync.cs
@@ -34,11 +34,10 @@ public class AnimSync: IDalamudPlugin {
 	private static Dictionary<nint, (Vector3, Quaternion, nint)> RootSyncs = new();
 	
 	public unsafe AnimSync() {
-		// AnimRootHook = HookProv.HookFromAddress<AnimRootDelegate>(SigScanner.ScanText("48 83 EC 08 8B 02"), AnimRoot);
+	 // AnimRootHook = HookProv.HookFromAddress<AnimRootDelegate>(SigScanner.ScanText("48 83 EC 08 8B 02"), AnimRoot);
 		AnimRootHook = HookProv.HookFromAddress<AnimRootDelegate>(SigScanner.ScanText("48 83 EC 18 80 79 ?? 00"), AnimRoot);
 		AnimRootHook.Enable();
 		
-		Interface.UiBuilder.Draw += Draw;
 		Commands.AddHandler(command, new CommandInfo(OnCommand) {
 			HelpMessage = "Resets all player and battlenpc animations to 0"
 		});
@@ -46,79 +45,9 @@ public class AnimSync: IDalamudPlugin {
 	
 	public void Dispose() {
 		AnimRootHook.Dispose();
-		
-		Interface.UiBuilder.Draw -= Draw;
 		Commands.RemoveHandler(command);
 	}
-	
-	private unsafe void Draw() {
-		lock(RootSyncs)
-			RootSyncs.Clear();
 		
-		foreach(var obj in Objects) {
-			if(IsValidObject(obj)) {
-				var actor = (Actor*)obj.Address;
-				var syncs = new List<(IGameObject, bool)>();
-				
-				foreach(var obj2 in Objects) {
-					if(IsValidObject(obj2)) {
-						var actor2 = (Actor*)obj2.Address;
-						
-						if(obj == obj2) continue;
-						if(actor->Control->hkaAnimationControl.Binding.ptr->Animation.ptr->Duration != actor2->Control->hkaAnimationControl.Binding.ptr->Animation.ptr->Duration) continue;
-						if(Math.Abs((obj2.Rotation - obj.Rotation + Math.PI) % (Math.PI * 2) - Math.PI) > MAXROT) continue;
-						if(Vector3.Distance(obj.Position, obj2.Position) > MAXDIST) continue;
-						
-						syncs.Add((obj2, actor->Control->hkaAnimationControl.LocalTime != actor2->Control->hkaAnimationControl.LocalTime));
-					}
-				}
-				
-				lock(RootSyncs) {
-					if(syncs.Count > 0) {
-						var transform = actor->DrawObject->Skeleton->Transform;
-						var position = (Vector3)transform.Position;
-						var rotations = new List<Quaternion>() {transform.Rotation};
-						
-						var time = actor->Control->hkaAnimationControl.LocalTime;
-						foreach(var (syncObj, syncTime) in syncs) {
-							transform = actor->DrawObject->Skeleton->Transform;
-							position += (Vector3)transform.Position;
-							rotations.Add(transform.Rotation);
-							
-							if(syncTime)
-								time = Math.Max(time, ((Actor*)syncObj.Address)->Control->hkaAnimationControl.LocalTime);
-						}
-						
-						position /= syncs.Count + 1;
-						
-						Quaternion rotation;
-						if(rotations.Count == 2)
-							rotation = Quaternion.Slerp(rotations[0], rotations[1], 0.5f);
-						else {
-							// Havent actually tested this but it *should* be okey at averaging 2+ player rotations with the lossy netcode that is xiv
-							var target = Quaternion.CreateFromAxisAngle(Vector3.UnitY, 0);
-							rotations.Sort((a, b) => Math.Abs(Quaternion.Dot(target, a)).CompareTo(Math.Abs(Quaternion.Dot(target, b))));
-							rotation = rotations[0];
-							for(int i = 1; i < rotations.Count; i++)
-								rotation = Quaternion.Slerp(rotation, rotations[i], 1f / (i + 1));
-						}
-						
-						
-						RootSyncs[(nint)actor->DrawObject->Skeleton->PartialSkeletons->GetHavokPose(0)] = (position, rotation, (nint)actor->DrawObject->Skeleton);
-						actor->Control->hkaAnimationControl.LocalTime = time;
-						
-						foreach(var (syncObj, syncTime) in syncs) {
-							RootSyncs[(nint)((Actor*)syncObj.Address)->DrawObject->Skeleton->PartialSkeletons->GetHavokPose(0)] = (position, rotation, (nint)((Actor*)syncObj.Address)->DrawObject->Skeleton);
-							if(syncTime)
-								((Actor*)syncObj.Address)->Control->hkaAnimationControl.LocalTime = time;
-						}
-							
-					}
-				}
-			}
-		}
-	}
-	
 	private unsafe void OnCommand(string cmd, string args) {
 		if(cmd != command)
 			return;
